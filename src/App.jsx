c637043c9a82c652ce017c6198008dc078f84f55
gustavoaso <gustavo.alcantara@aluno.ifsp.edu.br>
2026-03-10 07:32:40 -0300

    Iniciando Projeto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Topo from ./components/Topo
+
+function App() {
+  
+
+  return (
+      <div>
+       <header>
+        <Topo/>
+       </header>
+      </div>
+  )
+}
+
+export default App
